59209c410447ac10d4362eb550b0df2edd651eb5
Adam Peña <dev-no-reply@adamjpena.com>
2024-08-11 09:32:10 +0900

    update rollup

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,14 +1,25 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
+import babel from '@rollup/plugin-babel';
+import terser from '@rollup/plugin-terser';
 
 export default {
   input: 'src/index.ts',
   output: {
     file: 'dist/index.js',
-    format: 'es',
+    format: 'cjs',
     sourcemap: true,
   },
-  plugins: [resolve(), commonjs(), typescript()],
+  plugins: [
+    resolve(),
+    commonjs(),
+    typescript(),
+    babel({
+      babelHelpers: 'bundled',
+      extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    }),
+    terser(),
+  ],
   external: ['react'],
 };
